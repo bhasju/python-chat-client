4a6b8850242fb3ddf55cd8262397b67f98f865ae
bhasju <bhaskardutta31297@gmail.com>
2020-10-11 21:09:47 +0530

    removed some unnecessary files

---
+++ server.py
@@ -0,0 +1,119 @@
+import socket
+import select
+import threading
+#from chatroom import Chatroom
+
+HEADER_LENGTH = 10
+
+IP = "127.0.0.1"
+PORT = 1234
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.bind((IP, PORT))
+server_socket.listen()
+
+print(f'Listening for connections on {IP}:{PORT}...')
+
+sockets_list = [server_socket]
+
+#errorprone
+client_list = []
+message_list = []
+
+readable=[]
+writable=[]
+exceptional=[]
+
+class clients(threading.Thread):
+	def __init__(self, client_socket, client_address, client_username,client_inbox=[],client_outbox=[],disconnected=False):
+		threading.Thread.__init__(self)
+		threading.Thread.daemon=True
+		self.client_socket=client_socket
+		self.client_address=client_address
+		self.client_username=client_username
+		self.disconnected=False
+	
+	def	check_avl(self):
+		for client in exceptional:
+			if client == self.client_socket:
+				self.disconnected=True
+
+	def check_for_message(self):
+		
+		message=receive_message(self.client_socket)
+		if message==False:
+			self.disconnected=True
+		else:
+			chatroom.message_list.append({'sock':self.client_socket,'message': message})
+			# print(f"{self.client_username} says {message['data'].decode('utf-8')}")
+			# print(chatroom.message_list)
+			#test			
+
+	def run(self):
+		print(f"{self.client_username} client thread started")
+		while self.disconnected==False:
+			self.check_avl()
+			self.check_for_message()
+
+class Chatroom(threading.Thread):
+	def __init__(self):
+		threading.Thread.__init__(self)
+		threading.Thread.daemon=True
+		self.client_list=[]
+		self.message_list=[]
+
+
+	def run(self):
+		while True:
+			if not self.message_list:
+				continue
+			else:
+				for message in self.message_list:
+					for client in self.client_list:
+						if message['sock'] != client:
+							send_message(message['message'],client)
+				self.message_list.clear()			
+								
+
+
+
+def receive_message(sender_socket):
+
+	try:
+		message_header = sender_socket.recv(HEADER_LENGTH)
+		if not len(message_header):
+			return False
+
+		message_length = int(message_header.decode('utf-8').strip())
+		return {'header': message_header, 'data': sender_socket.recv(message_length)}
+		print("message received")
+	except:
+		return False
+
+
+
+def send_message(message_data,reciever_socket):
+	reciever_socket.send(message_data['header']+message_data['data'])
+
+
+def accept_new_connection(client_list, sockets_list):
+	client_socket, client_address = server_socket.accept()
+	client_username = receive_message(client_socket)
+	if client_username is False:
+		return
+	new_client=clients(client_socket=client_socket,client_address= client_address,client_username=client_username['data'].decode('utf-8'))	
+	client_list.append(client_socket)	
+	new_client.start()
+	sockets_list.append(client_socket)
+
+	print('Accepted new connection from {}:{}, username: {}'.format(*client_address, client_username['data'].decode('utf-8')))
+
+chatroom=Chatroom()
+chatroom.start()
+while True:
+	readable, writable, exceptional = select.select(sockets_list, [], sockets_list)
+	for notified_socket in readable:
+		if notified_socket==server_socket:
+			accept_new_connection(chatroom.client_list, sockets_list)
+		
